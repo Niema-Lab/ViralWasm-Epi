987811d50de4676091c799ebfe5b1f330148157a
Daniel Ji <jidaniel1234@gmail.com>
2023-09-11 20:08:22 -0700

    debug vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '')
+	console.log(env.VITE_OFFLINE_VERSION === 'true' ? '/' : '/ViralWasm-Epi/')
 	return {
-		base: '/ViralWasm-Epi/',
+		base: env.VITE_OFFLINE_VERSION === 'true' ? '/' : '/ViralWasm-Epi/',
 		plugins: [react()],
 		build: {
 			sourcemap: true,
