afe4a2a02ce8294bc588bb5c5d4e9ab11aa5cab8
Daniel Ji <jidaniel1234@gmail.com>
2023-10-02 12:38:23 -0700

    speed up workflow

--- e2e/tests/run-baseline-benchmarks.sh
+++ e2e/tests/run-baseline-benchmarks.sh
@@ -7,7 +7,6 @@ run_benchmark() {
 	OUT_DIR=../../benchmarks/$1/cli/
 	rm -rf $OUT_DIR
 
-	minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam MT072688.fasta.mmi $2
 	/usr/bin/time -v ViralMSA.py -e email@address.com -s sequence.fas.sam -o $OUT_DIR -r MT072688.fasta --viralmsa_dir cache 2>time_output.log
 
 	grep "User time (seconds): " time_output.log | awk '{print $4}' > "$OUT_DIR/time.log"
@@ -15,11 +14,12 @@ run_benchmark() {
 
 	rm -rf cache
 	rm -rf time_output.log
-	rm -rf sequence.fas.sam
 }
 
 for n in 100 200 400 1000 2000 4000 ; do 
+	minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam MT072688.fasta.mmi "$n.01.true.fas"
 	for r in {1..10}; do 
 		run_benchmark "$n.$r" "$n.01.true.fas"
 	done
+	rm -rf sequence.fas.sam
 done
