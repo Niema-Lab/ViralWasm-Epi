f8ac72bd2e797458132855863ec8df8e86a55f35
Daniel Ji <jidaniel1234@gmail.com>
2023-10-02 12:27:52 -0700

    repeat benchmarks

--- e2e/tests/download-benchmark-data.sh
+++ e2e/tests/download-benchmark-data.sh
@@ -1,15 +1,19 @@
 SCRIPTPATH=$(dirname "$PWD")
 
-# Download reads.fastq.gz file (1 milion sequences)
 cd $SCRIPTPATH
+rm -rf data
 mkdir data
 cd data
-curl -LO https://github.com/niemasd/ViralMSA-Paper/raw/master/data/SARSCOV2/40000/40000.01.true.fas.gz
-gzip -d -f 40000.01.true.fas.gz
-curl -LO https://github.com/niemasd/ViralMSA-Paper/raw/master/data/SARSCOV2/4000/4000.01.true.fas.gz
-gzip -d -f 4000.01.true.fas.gz
-curl -LO https://github.com/niemasd/ViralMSA-Paper/raw/master/data/SARSCOV2/400/400.01.true.fas.gz
-gzip -d -f 400.01.true.fas.gz
+
+for n in 100 200 400 1000 2000 4000; do
+	curl -LO https://github.com/niemasd/ViralMSA-Paper/raw/master/data/SARSCOV2/$n/$n.01.true.fas.gz
+	gzip -d -f $n.01.true.fas.gz
+done
+
 curl -LO https://raw.githubusercontent.com/niemasd/ViralMSA-Paper/master/data/SARSCOV2/MT072688.fasta
 curl -LO https://raw.githubusercontent.com/niemasd/viralmsa/master/ref_genomes/NC_001802/NC_001802.fas
 curl -LO https://raw.githubusercontent.com/niemasd/viralmsa/master/example/example_hiv.fas
+
+# index reference
+minimap2 -t 1 -d NC_001802.fas.mmi NC_001802.fas
+minimap2 -t 1 -d MT072688.fasta.mmi MT072688.fasta
--- e2e/tests/run-baseline-benchmarks.sh
+++ e2e/tests/run-baseline-benchmarks.sh
@@ -1,61 +1,25 @@
 cd ../data
+rm -rf ../../benchmarks
+mkdir ../../benchmarks
+# $1: output directory
+# $2: reads file
+run_benchmark() {
+	OUT_DIR=../../benchmarks/$1/cli/
+	rm -rf $OUT_DIR
 
-# index reference
-minimap2 -t 1 -d NC_001802.fas.mmi NC_001802.fas
-minimap2 -t 1 -d MT072688.fasta.mmi MT072688.fasta
+	minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam MT072688.fasta.mmi $2
+	/usr/bin/time -v ViralMSA.py -e email@address.com -s sequence.fas.sam -o $OUT_DIR -r MT072688.fasta --viralmsa_dir cache 2>time_output.log
 
-### TEST #1: Example data
-OUT_DIR=../../benchmarks/example/cli/
-rm -rf $OUT_DIR
+	grep "User time (seconds): " time_output.log | awk '{print $4}' > "$OUT_DIR/time.log"
+	grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' > "$OUT_DIR/memory.log"
 
-minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam NC_001802.fas.mmi example_hiv.fas
-/usr/bin/time -v ViralMSA.py -e email@address.com -s sequence.fas.sam -o $OUT_DIR -r NC_001802.fas --viralmsa_dir cache 2>time_output.log
+	rm -rf cache
+	rm -rf time_output.log
+	rm -rf sequence.fas.sam
+}
 
-grep "User time (seconds): " time_output.log | awk '{print $4}' > "$OUT_DIR/time.log"
-grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' > "$OUT_DIR/memory.log"
-
-rm -rf cache
-rm -rf time_output.log
-rm -rf sequence.fas.sam
-
-### TEST #2: 400 reads
-OUT_DIR=../../benchmarks/400/cli/
-rm -rf $OUT_DIR
-
-minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam MT072688.fasta.mmi '400.01.true.fas'
-/usr/bin/time -v ViralMSA.py -e email@address.com -s sequence.fas.sam -o $OUT_DIR -r MT072688.fasta --viralmsa_dir cache 2>time_output.log
-
-grep "User time (seconds): " time_output.log | awk '{print $4}' > "$OUT_DIR/time.log"
-grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' > "$OUT_DIR/memory.log"
-
-rm -rf cache
-rm -rf time_output.log
-rm -rf sequence.fas.sam
-
-### TEST #3: 4000 reads
-OUT_DIR=../../benchmarks/4000/cli/
-rm -rf $OUT_DIR
-
-minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam MT072688.fasta.mmi '4000.01.true.fas'
-/usr/bin/time -v ViralMSA.py -e email@address.com -s sequence.fas.sam -o $OUT_DIR -r MT072688.fasta --viralmsa_dir cache 2>time_output.log
-
-grep "User time (seconds): " time_output.log | awk '{print $4}' > "$OUT_DIR/time.log"
-grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' > "$OUT_DIR/memory.log"
-
-rm -rf cache
-rm -rf time_output.log
-rm -rf sequence.fas.sam
-
-### TEST #4: 40000 reads
-OUT_DIR=../../benchmarks/40000/cli/
-rm -rf $OUT_DIR
-
-minimap2 -t 1 --score-N=0 --secondary=no --sam-hit-only -a -o sequence.fas.sam MT072688.fasta.mmi '40000.01.true.fas'
-/usr/bin/time -v ViralMSA.py -e email@address.com -s sequence.fas.sam -o $OUT_DIR -r MT072688.fasta --viralmsa_dir cache 2>time_output.log
-
-grep "User time (seconds): " time_output.log | awk '{print $4}' > "$OUT_DIR/time.log"
-grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' > "$OUT_DIR/memory.log"
-
-rm -rf cache
-rm -rf time_output.log
-rm -rf sequence.fas.sam
+for n in 100 200 400 1000 2000 4000 ; do 
+	for r in {1..10}; do 
+		run_benchmark "$n.$r" "$n.01.true.fas"
+	done
+done
