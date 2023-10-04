ece385df3e25f14a7eb0efea9e6df63df493721e
Daniel Ji <jidaniel1234@gmail.com>
2023-10-03 21:07:49 -0700

    improve benchmark logging

--- e2e/tests/run-baseline-benchmarks.sh
+++ e2e/tests/run-baseline-benchmarks.sh
@@ -9,17 +9,18 @@ run_benchmark() {
 	/usr/bin/time -v ViralMSA.py -e email@address.com -s $2 -o $OUT_DIR -r MT072688.fasta --viralmsa_dir cache 2>time_output.log
 
 	mkdir -p $LOG_DIR
-	grep "User time (seconds): " time_output.log | awk '{print $4}' > "$LOG_DIR/time.log"
+	grep "User time (seconds): " time_output.log | awk '{print $4}' >"$LOG_DIR/time.log"
 	echo "$LOG_DIR/time.log"
+	ls -lh "$OUT_DIR"
 	cat "$LOG_DIR/time.log"
-	grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' > "$LOG_DIR/memory.log"
+	grep "Maximum resident set size (kbytes): " time_output.log | awk '{print $6}' >"$LOG_DIR/memory.log"
 
 	rm -rf cache
 	rm -rf time_output.log
 }
 
-for n in 100 200 400 1000 2000 4000 ; do 
-	for r in {1..10}; do 
+for r in {1..10}; do
+	for n in 100 200 400 1000 2000 4000; do
 		run_benchmark "$n.$r" "$n.fas.sam"
 	done
 done
