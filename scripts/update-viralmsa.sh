986f76f9368683aef79cfecdb9096e90fef1b8e1
Daniel Ji <jidaniel1234@gmail.com>
2023-11-06 13:22:49 -0800

    fix viralmsa update script

--- scripts/update-viralmsa.sh
+++ scripts/update-viralmsa.sh
@@ -12,8 +12,10 @@ wget https://raw.githubusercontent.com/niemasd/ViralMSA/master/ViralMSA.py
 # update data
 cd $CUR_DIR
 cd $VIRALMSA_DATA_DIR
+rm -rf repo_structure.json
 wget https://api.github.com/repos/niemasd/ViralMSA/git/trees/master?recursive=1 -O repo_structure.json
 cd ref_genomes
+rm -rf *
 
 curl https://codeload.github.com/niemasd/ViralMSA/tar.gz/master |
 	tar -xz --strip=2 ViralMSA-master/ref_genomes
