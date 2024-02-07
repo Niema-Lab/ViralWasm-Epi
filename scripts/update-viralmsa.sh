1b27e92bfef024f798411d16e8a666ca14d0a754
Daniel Ji <jidaniel1234@gmail.com>
2024-02-06 16:20:38 -0800

    attempt to fix

--- scripts/update-viralmsa.sh
+++ scripts/update-viralmsa.sh
@@ -15,6 +15,7 @@ cd $VIRALMSA_DATA_DIR
 rm -rf Reference-Genomes/
 git clone https://github.com/Niema-Lab/Reference-Genomes.git
 cd Reference-Genomes
-rm -rf *.github/
+rm -rf .github/
+rm -rf .git/
 python3 compile.py
 mv REFS.json ../REFS.json
